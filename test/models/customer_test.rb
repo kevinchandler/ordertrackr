b70d61464703516ebb3c89df85e3118c3ff1687d
Kevin Chandler <im.kevin@me.com>
2016-12-23 00:33:42 -0500

    spec failure message

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -15,8 +15,8 @@ class CustomerTest < ActiveSupport::TestCase
 
   test 'it should standardize the phone when saving' do
     customer = customers :valid_customer
-    customer.phone = '13404053188'
+    customer.phone = '13404053188fff'
     customer.save
-    assert_equal '3404053188', customer.phone
+    assert_equal '3404053188', customer.phone, 'customer phone not standardized'
   end
 end
