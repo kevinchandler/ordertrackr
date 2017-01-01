0760e3702052832d8b8f4007e6b3dc20590201b0
Kevin Chandler <im.kevin@me.com>
2017-01-01 01:08:46 -0500

    seed db in non-prod environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+if Rails.env.production?
+  raise 'Database cannot be seeded in production environment'
+end
+
+Customer.delete_all
+Order.delete_all
+DeliveryAddress.delete_all
+
+customer = Customer.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  phone: '1234567890'
+)
+
+delivery_address = DeliveryAddress.create!(
+  street_address: Faker::Address.street_address,
+  city: Faker::Address.city,
+  state: Faker::Address.state,
+  zipcode: Faker::Address.zip,
+  latitude: Faker::Address.latitude,
+  longitude: Faker::Address.longitude
+)
+
+# TODO: seed agent and driver once models are created, update ids below
+order = Order.create!(
+  agent_id: 1,
+  driver_id: 1,
+  customer_id: customer.id,
+  delivery_address_id: delivery_address.id,
+  complete: false,
+)
