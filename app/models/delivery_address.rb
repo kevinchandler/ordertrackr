37de47c0fbd56e72a79fbcb675a8a1bbf3c8c942
Kevin Chandler <im.kevin@me.com>
2017-01-03 18:31:47 -0500

    coordinates method

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -5,6 +5,10 @@ class DeliveryAddress < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: -> { latitude.nil? && longitude.nil? }
 
+  def coordinates
+    "#{latitude},#{longitude}" if latitude && longitude
+  end
+
   private
 
   def address
--- test/models/delivery_address_test.rb
+++ test/models/delivery_address_test.rb
@@ -4,12 +4,24 @@ class DeliveryAddressTest < ActiveSupport::TestCase
   test 'it should require a street_address' do
     delivery_address = delivery_addresses :los_angeles
     delivery_address.street_address = nil
-    assert_not delivery_address.save, 'delivery_address street_address not required when it should be'
+    assert_not(
+      delivery_address.save,
+      'delivery_address street_address not required when it should be'
+    )
   end
 
   test 'it should require a zipcode' do
     delivery_address = delivery_addresses :los_angeles
     delivery_address.zipcode = nil
     assert_not delivery_address.save, 'customer zipcode not required when it should be'
+  end
+
+  test '#coordinates' do
+    delivery_address = delivery_addresses :los_angeles
+    assert_equal(
+      delivery_address.coordinates,
+      '34.0522,-118.2437',
+      'Delivery address coordinates not correct'
+    )
   end
 end
