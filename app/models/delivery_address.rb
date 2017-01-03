7598ca23fffcae6fbdd82e55d1298070c46dd153
Kevin Chandler <im.kevin@me.com>
2017-01-03 18:25:56 -0500

    has_many orders

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,5 +1,6 @@
 class DeliveryAddress < ApplicationRecord
   validates :street_address, :zipcode, presence: true
+  has_many :orders
 
   geocoded_by :address
   after_validation :geocode, if: -> { latitude.nil? && longitude.nil? }
